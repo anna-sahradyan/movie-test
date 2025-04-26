5ccd04a0aa41e1c085e2b30a0305dd71910e1c96
Anna Paruryan <a.sahradyan72@gmail.com>
2025-04-26 12:11:29 +0400

    updated

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import  style from './header.module.scss';
+import {useAppDispatch} from "../../redux/store.ts";
+import {fetchPopular} from "../../redux/slices/thunks/thunk.ts";
+import {setCurrentPage, setSearchTerm} from "../../redux/slices/movieSlice.ts";
 
 export const Header: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+
+    const handleClickFilms = () => {
+        dispatch(setSearchTerm(''));
+        dispatch(setCurrentPage(1));
+        dispatch(fetchPopular(1));
+        navigate('/');
+    };
     return (
         <header className={style.header}>
             <div className = {style.inner}>
@@ -13,8 +25,8 @@ export const Header: React.FC = () => {
             </div>
             <nav className={style.nav}>
                 <ul>
-                    <li><Link to="/" className={style.navLink}>Главная</Link></li>
-                    <li><Link to="#" className={style.navLink}>Фильмы</Link></li>
+                    <li><Link to="/" className={style.navLink} >Главная</Link></li>
+                    <li><Link to="#" className={style.navLink} onClick={handleClickFilms}>Фильмы</Link></li>
                 </ul>
             </nav>
             </div>
