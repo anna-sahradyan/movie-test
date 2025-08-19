0337aa6a3884a7ee9da1653cc90e760fad859ab9
anna-sahradyan <a.sahradyan72@gmail.com>
2025-08-19 23:02:09 +0400

    updated

--- src/components/card/MovieCard.tsx
+++ src/components/card/MovieCard.tsx
@@ -1,24 +1,24 @@
-import React, {useState} from "react";
-import {Movie} from "../../redux/slices/thunks/types.ts";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Movie } from "../../redux/slices/thunks/types.ts";
+import { Link } from "react-router-dom";
 import style from './card.module.scss';
 
 interface MovieCardProps {
     movie: Movie;
 }
 
-export const MovieCard: React.FC<MovieCardProps> = ({movie}) => {
+export const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
     const [isLoaded, setIsLoaded] = useState(false);
 
     const imageUrl = movie.poster_path
         ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-        : '/clip.jpg';
+        : '/clip.jpg'; // fallback картинка
 
     return (
-        <div key={movie.id} className={style.movieItem}>
+        <div className={style.movieItem}>
             <Link to={`/movie/${movie.id}`} className={style.movieCardLink}>
                 <div className={style.imageWrapper}>
-                    {!isLoaded && <div className={style.skeleton}/>} {/* Плейсхолдер */}
+                    {!isLoaded && <div className={style.skeleton} />}
                     <img
                         src={imageUrl}
                         alt={movie.title}
@@ -30,7 +30,7 @@ export const MovieCard: React.FC<MovieCardProps> = ({movie}) => {
                 <div className={style.movieInfo}>
                     <h3 className={style.movieTitle}>{movie.title}</h3>
                     <p className={style.movieOverview}>{movie.overview}</p>
-                    <p className={style.movieRating}>Рейтинг: {movie.vote_average}</p>
+                    <p className={style.movieRating}>⭐ {movie.vote_average}</p>
                 </div>
             </Link>
         </div>
